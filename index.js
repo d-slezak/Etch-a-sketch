181a6c840dc745e8f1193cbfc3b298a896c0ac94
d.slezak <Slezak.Dan@outlook.com>
2022-05-30 17:22:22 -0400

    add border, add clear button

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 const board = document.getElementById('board');
-const squareColor = 'red';
+let squareColor = 'red';
+let backgroundColor = 'black';
 
 let numSquares = 16;
 size = (1/numSquares)*100;
 
-for (let i = 0; i < numSquares; i++)
-{
-    for (let j = 0; j < numSquares; j++)
+function buildBoard() {
+    for (let i = 0; i < numSquares; i++)
     {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.style.backgroundColor = 'black';
-        square.style.width = `${size}%`;
-        square.style.paddingTop = `${size}%`;
-        board.appendChild(square);
+        for (let j = 0; j < numSquares; j++)
+        {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.backgroundColor = backgroundColor;
+            square.style.width = `${size}%`;
+            square.style.paddingTop = `${size}%`;
+            board.appendChild(square);
+        }
     }
+
+    let gridSquares = board.querySelectorAll('div');
+    gridSquares.forEach(square => square.addEventListener('mouseover', colorSquares));
+
 }
 
-let gridSquares = board.querySelectorAll('div');
-gridSquares.forEach(square => square.addEventListener('mouseover', colorSquares));
+function changeColor(color)
+{
+    squareColor = color;
+}
+
+function resetBoard() {
+    board.innerHTML = '';
+    buildBoard();
+}
 
 function colorSquares () {
     this.style.backgroundColor = squareColor;
 }
+
+let eraserBtn = document.getElementById('eraser');
+eraserBtn.addEventListener('click', changeColor('black'));
+
+let clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', resetBoard);
+
+buildBoard();
